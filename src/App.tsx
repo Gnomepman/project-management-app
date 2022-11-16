bbf5c423dbf51bd82fa5df1625c0a3448cd04e9e
Danya <danilomaster2003@ukr.net>
2022-11-16 23:52:24 +0200

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { Error } from './pages/ErrorPage/ErrorPage';
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import './App.css';
+import { BoardsPage } from './pages/BoardsPage/BoardsPage';
+import { Board } from './components/Board/Board';
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
+        {/* <Route path="/" element={ }></Route>  */}
+        <Route path="/boards" element={<BoardsPage />}></Route>
+        <Route path="/boards/:id" element={<Board />}></Route>
+        {/* <Route path="/registration" element={}></Route> */}
+        {/* <Route path="/login" element={}></Route> */}
         <Route path="*" element={<Error />} />
       </Routes>
     </div>
