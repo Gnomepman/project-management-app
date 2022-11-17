0f7ec53de8895cf7ab739f4e6f23470606cf42a6
ekater1na <ekaterina_l@tut.by>
2022-11-17 22:40:14 +0300

    feat: change images styles

--- src/components/TechnologyItem/TechnologyItem.tsx
+++ src/components/TechnologyItem/TechnologyItem.tsx
@@ -11,7 +11,7 @@ export const TechnologyItem = ({ item }: TechnologyItemProps) => {
   return (
     <Card className="col mx-1">
       <a href={item.url} target="_blank" rel="noreferrer">
-        <img className="tech-img rounded-2" src={item.logo_url} alt={item.title} />
+        <img height="70" className="tech-img rounded-2" src={item.logo_url} alt={item.title} />
         <h5 className="d-none d-sm-block d-sm-none d-md-block">{item.title}</h5>
       </a>
     </Card>
