0a15ac0fac7c92b48a6840ceffd33aa8ffbd21c3
Danya <danilomaster2003@ukr.net>
2022-11-20 13:59:31 +0200

    update: update Boards page

--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -1,20 +1,68 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import './BoardsPage.scss';
 
 export function BoardsPage() {
   //const { boards, setBoards } = useState([]);
-  const temp = 123;
+  const { t } = useTranslation();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    //call modal window to get title from user and create board with such title
+    e.preventDefault();
+    console.log('creating new board');
+  };
 
   useEffect(() => {
     //fetch boards and setBoards()
   });
+
   return (
     <>
-      <div>Here will be boards</div>
-      {/* {boards.map((elem) => { */}
-      <Link to={`/boards/${temp}`}>testing board</Link>
-
-      {/* })} */}
+      <div className="container-xxl min-vh-100">
+        <section className="boards container mt-2 mb-2">
+          <Button className="fw-semibold" variant="outline-info" onClick={handleShow}>
+            {t('boards.create')}
+          </Button>
+          {/* {boards.map((elem) => { */}
+          <Link to={`/boards/123`}>
+            <Button className="board">
+              {/* add limit for the name, if length is to high - cut string and add '...' to the end */}
+              <span className="board-name fw-semibold fs-5">Board name</span>
+            </Button>
+          </Link>
+          {/* })} */}
+        </section>
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>{t('boards.modal.creating')}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label>{t('boards.modal.form.title')}</Form.Label>
+                <Form.Control
+                  type="name"
+                  placeholder={String(t('boards.modal.form.placeholder'))}
+                />
+              </Form.Group>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  {t('boards.modal.close')}
+                </Button>
+                <Button variant="primary" onClick={handleClose} type="submit">
+                  {t('boards.modal.create')}
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </div>
     </>
   );
 }
