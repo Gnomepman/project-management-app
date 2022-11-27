0b77979a31c6d7fdfcf0fd5bcda8d47d3a222d2d
Judex66 <sermac2000@mail.ru>
2022-11-27 18:34:26 +0300

    feat: add modal with for edit users data

--- src/pages/EditProfilePage/EditProfilePage.tsx
+++ src/pages/EditProfilePage/EditProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { ErrorComponent } from '../../components/Error/ErrorComponent';
 import { IErrorMessage, IUser } from '../../models';
@@ -16,6 +16,7 @@ import {
 } from '../../store/api/userApi';
 
 export const EditProfilePage = () => {
+  const [modal, setModalData] = useState(false);
   const { t } = useTranslation();
 
   const navigate = useNavigate();
@@ -64,7 +65,7 @@ export const EditProfilePage = () => {
 
   if (isLoading) return <Loader />;
   if (isError) return <ErrorComponent message={(error as IErrorMessage).data.message} />;
-
+  const onClose = () => setModalData(false);
   return (
     <>
       {data && (
@@ -72,48 +73,53 @@ export const EditProfilePage = () => {
           <h5>Logged User:</h5>
           <p>name: {data?.name}</p>
           <p>login: {data?.login}</p>
-          <Button>Change User data</Button>
+          <Button
+            onClick={() => {
+              setModalData(true);
+              console.log(modal);
+            }}
+          >
+            Change User data
+          </Button>
           <Button onClick={() => deleteUser(id).then(() => navigate('/login'))}>
             Delete user data
           </Button>
         </div>
       )}
-      <Modal>
-        <div className="container app-container">
-          <div className="row d-flex pt-5 justify-content-center">
-            <div className="col-md-4">
-              <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
-                <div className="form-outline mb-4">
-                  <FormInput
-                    field="login"
-                    title={t('auth.login')}
-                    register={register}
-                    errors={errors.login}
-                  />
-                  <FormInput
-                    field="name"
-                    title={t('auth.name')}
-                    register={register}
-                    errors={errors.name}
-                  />
-                  <FormInput
-                    field="password"
-                    title={t('auth.password')}
-                    register={register}
-                    errors={errors.password}
-                  />
-                  <Button
-                    type="submit"
-                    value="submit"
-                    data-testid="button-submit"
-                    disabled={hasError()}
-                    className={hasError() ? 'bg-secondary my-4' : 'bg-primary my-4'}
-                  >
-                    {t('auth.submit')}
-                  </Button>
-                </div>
-              </form>
-            </div>
+      <Modal show={modal} onHide={onClose}>
+        <div className="row d-flex pt-5 justify-content-center">
+          <div className="col-md-4">
+            <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
+              <div className="form-outline mb-4">
+                <FormInput
+                  field="login"
+                  title={t('auth.login')}
+                  register={register}
+                  errors={errors.login}
+                />
+                <FormInput
+                  field="name"
+                  title={t('auth.name')}
+                  register={register}
+                  errors={errors.name}
+                />
+                <FormInput
+                  field="password"
+                  title={t('auth.password')}
+                  register={register}
+                  errors={errors.password}
+                />
+                <Button
+                  type="submit"
+                  value="submit"
+                  data-testid="button-submit"
+                  disabled={hasError()}
+                  className={hasError() ? 'bg-secondary my-4' : 'bg-primary my-4'}
+                >
+                  {t('auth.submit')}
+                </Button>
+              </div>
+            </form>
           </div>
         </div>
       </Modal>
