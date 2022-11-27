c7fb02dda1e067cf19bb6714c6f31fd4a7f94f35
Danya <danilomaster2003@ukr.net>
2022-11-27 10:15:24 +0200

    feat: add boards to page using api

--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -3,41 +3,64 @@ import { Button, Modal } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useGetBoardsQuery } from '../../store/api/boardApi';
+import { stringToColour } from '../../utils/colorFromString';
 import './BoardsPage.scss';
 
 export function BoardsPage() {
-  //const { boards, setBoards } = useState([]);
   const { t } = useTranslation();
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const { data, isLoading } = useGetBoardsQuery();
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     //call modal window to get title from user and create board with such title
     e.preventDefault();
-    console.log('creating new board');
+    console.log('creating new board', e);
   };
 
   useEffect(() => {
     //fetch boards and setBoards()
   });
+  if (isLoading)
+    return (
+      <>
+        <p>loading...</p>
+      </>
+    );
 
   return (
     <>
       <div className="container-xxl min-vh-100">
-        <section className="boards container mt-2 mb-2">
-          <Button className="fw-semibold" variant="outline-info" onClick={handleShow}>
-            {t('boards.create')}
-          </Button>
-          {/* {boards.map((elem) => { */}
-          <Link to={`/boards/123`}>
-            <Button className="board">
-              {/* add limit for the name, if length is to high - cut string and add '...' to the end */}
-              <span className="board-name fw-semibold fs-5">Board name</span>
+        <div className="boards container mt-2 mb-2">
+          <>
+            <Button className="fw-semibold" variant="outline-info" onClick={handleShow}>
+              {t('boards.create')}
             </Button>
-          </Link>
-          {/* })} */}
-        </section>
+            {data &&
+              data!.map((elem, index) => {
+                return (
+                  <>
+                    <Link to={`/boards/${elem._id}`} key={index}>
+                      <Button
+                        className="board"
+                        style={{
+                          backgroundImage: `linear-gradient(110deg, ${stringToColour(
+                            elem._id!
+                          )}, ${stringToColour(elem._id!.split('').reverse().join())}`,
+                          opacity: 0.8,
+                        }}
+                      >
+                        {/* add limit for the name, if length is to high - cut string and add '...' to the end */}
+                        <span className="board-name fw-semibold fs-5">{elem.title}</span>
+                      </Button>
+                    </Link>
+                  </>
+                );
+              })}
+          </>
+        </div>
         <Modal show={show} onHide={handleClose} centered>
           <Modal.Header closeButton>
             <Modal.Title>{t('boards.modal.creating')}</Modal.Title>
