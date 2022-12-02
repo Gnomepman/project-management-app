511856daca73ebec920b46dba5e601220f98ca5f
danyil <danilomaster2003@ukr.net>
2022-12-02 09:47:29 +0200

    fix: footer height

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,17 +11,17 @@ export const Footer = () => {
     <footer className="bg-light">
       <div className="container-xxl">
         <section className="d-flex py-2 justify-content-between align-items-center">
-          <div className="d-xl-flex d-md-flex col-3 py-3">
+          <div className="d-xl-flex d-md-flex col-3 py-1">
             <a href="https://rs.school/react/" target="_blank" rel="noreferrer">
-              <img height="37" src={RSSLogo} alt="rss" />
+              <img height="35" src={RSSLogo} alt="rss" />
             </a>
           </div>
           <div className="col-6">
             <TeamMembers />
           </div>
-          <div className="text-center col-3">
+          <span className="text-center col-3">
             © {year} | {t('footer.rights')}{' '}
-          </div>
+          </span>
         </section>
       </div>
     </footer>
