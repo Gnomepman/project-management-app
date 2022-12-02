721831da53376a81e1d6255707dd91291c4fccc4
ekater1na <ekaterina_l@tut.by>
2022-12-02 23:52:00 +0300

    feat: add header button

--- src/components/AuthSection/AuthSection.tsx
+++ src/components/AuthSection/AuthSection.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useActions } from '../../hooks/actions';
 import { Button, Nav, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { isAuthenticated } from '../../utils/isAuthenticated';
+import { ModalComponent } from '../ModalComponent/ModalComponent';
+import { CreateBoardModal } from '../CreateBoard/CreateBoardModal';
 
 export const AuthSection = () => {
   const { t } = useTranslation();
@@ -17,6 +19,8 @@ export const AuthSection = () => {
     localStorage.removeItem('token');
     navigate('/');
   };
+
+  const [createBoardModal, setCreateBoardModal] = useState(false);
 
   return (
     <>
@@ -32,27 +36,42 @@ export const AuthSection = () => {
       )}
 
       {isAuthenticated() && (
-        <Navbar>
-          <NavLink className="mr-10 px-2" to="/boards">
-            <Button variant="outline-danger btn-overflow"> {t('auth.add-boards')}</Button>
-          </NavLink>
+        <>
+          <Navbar>
+            <NavLink className="mr-10 px-2" to="/boards">
+              <Button
+                variant="outline-danger btn-overflow"
+                onClick={() => setCreateBoardModal(true)}
+              >
+                {t('auth.add-boards')}
+              </Button>
+            </NavLink>
 
-          <NavLink className="px-2 d-none d-md-block lh-sm" to="/">
-            <Button variant="outline-danger btn-overflow"> {t('auth.to-main-page')}</Button>
-          </NavLink>
+            <NavLink className="px-2 d-none d-md-block lh-sm" to="/">
+              <Button variant="outline-danger btn-overflow"> {t('auth.to-main-page')}</Button>
+            </NavLink>
 
-          <NavLink className="px-2" to="/edit-profile">
-            <Button variant="outline-secondary w-max-25 btn-overflow">
-              {t('auth.edit-profile')}
-            </Button>
-          </NavLink>
+            <NavLink className="px-2" to="/edit-profile">
+              <Button variant="outline-secondary w-max-25 btn-overflow">
+                {t('auth.edit-profile')}
+              </Button>
+            </NavLink>
 
-          <Nav.Item>
-            <Button className="btn-overflow" onClick={onClickHandler}>
-              {t('auth.sign-out')}
-            </Button>
-          </Nav.Item>
-        </Navbar>
+            <Nav.Item>
+              <Button className="btn-overflow" onClick={onClickHandler}>
+                {t('auth.sign-out')}
+              </Button>
+            </Nav.Item>
+          </Navbar>
+          <ModalComponent
+            show={createBoardModal}
+            title={t('boards.modal.creating')}
+            onHide={() => setCreateBoardModal(false)}
+            setModal={setCreateBoardModal}
+          >
+            <CreateBoardModal setCreateBoardModal={setCreateBoardModal} />
+          </ModalComponent>
+        </>
       )}
     </>
   );
