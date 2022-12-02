1ce7832101e4612dd3621ff315561341eaab3cc3
ekater1na <ekaterina_l@tut.by>
2022-12-02 18:17:36 +0300

    feat: update welcome page

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -8,15 +8,15 @@ export const Introduction = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="row justify-content-between py-1">
-      <div className="col-lg-3 col-md-2 pt-5 pt-md-0 pt-sm-0">
+    <section className="row justify-content-center py-1">
+      <div className="col-lg-3 col-md-2 pt-lg-5 pt-md-0 mt-lg-5 mt-md-0 text-center">
         <h1 className="pt-lg-5 pt-md-3 fw-bold">PM Guru</h1>
         <h4 className="py-2 py-sm-2">{t('wp.description')}</h4>
         <NavLink to="/boards">
           <Button variant="primary">{t('wp.button')}</Button>
         </NavLink>
       </div>
-      <div className="col-8 d-none d-sm-block px-2">
+      <div className="col-8 d-none d-sm-block px-2 py-1">
         <img className="w-100" src={MainImage} alt="pm-guru" />
       </div>
     </section>
