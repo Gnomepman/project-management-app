64603bfc44a19dd521d8e801daa42fa100987375
ekater1na <ekaterina_l@tut.by>
2022-12-04 17:23:10 +0300

    feat: update welcome-page

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -3,19 +3,33 @@ import { ITechnology } from '../../models';
 import { TechnologiesData } from '../../assets/data';
 import { TechnologyItem } from '../TechnologyItem/TechnologyItem';
 import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export const Technologies = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="container">
-      <div className="row gap-1  justify-content-center py-2">
-        <h5 className="text-center my-3">{t('wp.created')}</h5>
+    <>
+      <section className="row justify-content-center py-md-5 py-sm-3">
+        <div className="col-8 px-2 py-5">
+          <div className="row justify-content-center gap-3">
+            {TechnologiesData.map((item: ITechnology) => (
+              <TechnologyItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
 
-        {TechnologiesData.map((item: ITechnology) => (
-          <TechnologyItem key={item.id} item={item} />
-        ))}
-      </div>
-    </section>
+        <div className="col-lg-3 col-md-2 pt-lg-5 pt-md-0 mt-lg-5 mt-md-0 text-center">
+          <h1 className="fw-bold">{t('title')}</h1>
+          <h4 className="py-2 py-sm-2">{t('wp.created')}</h4>
+          <NavLink to="/boards">
+            <Button className="btn-lg" variant="primary">
+              {t('wp.button')}
+            </Button>
+          </NavLink>
+        </div>
+      </section>
+    </>
   );
 };
