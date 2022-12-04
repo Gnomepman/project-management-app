25bb9d56a0fd223821f876743c760588a5a9ece9
danyil <danilomaster2003@ukr.net>
2022-12-04 18:30:19 +0200

    fix: add gaps between loading spinners

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,7 +4,7 @@ import Spinner from 'react-bootstrap/Spinner';
 export const Loader = () => {
   return (
     <div className="app-container">
-      <div className="text-center py-5">
+      <div className="text-center py-5 d-flex gap-1 justify-content-center">
         <Spinner animation="border" variant="primary" />
         <Spinner animation="border" variant="success" />
         <Spinner animation="border" variant="danger" />
