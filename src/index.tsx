09090b857afcdc26dbb86a324acac381250d2541
ekater1na <ekaterina_l@tut.by>
2022-12-04 18:19:14 +0300

    feat: add snowfall

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,19 @@ import './i18n';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import Snowfall from 'react-snowfall';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+      <Snowfall
+        style={{
+          position: 'fixed',
+          width: '100vw',
+          height: '100vh',
+        }}
+      />
       <App />
     </BrowserRouter>
   </Provider>
