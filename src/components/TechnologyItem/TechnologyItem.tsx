445b0f988beb956b070968cc353d972ea46cf66c
ekater1na <ekaterina_l@tut.by>
2022-12-05 22:40:08 +0300

    fix: fix images size

--- src/components/TechnologyItem/TechnologyItem.tsx
+++ src/components/TechnologyItem/TechnologyItem.tsx
@@ -10,7 +10,12 @@ export const TechnologyItem = ({ item }: TechnologyItemProps) => {
   return (
     <div className="card-item col-3 py-3">
       <a href={item.url} target="_blank" rel="noreferrer">
-        <img height="60" className="tech-img rounded-2" src={item.logo_url} alt={item.title} />
+        <img
+          height="60"
+          className="tech-img rounded-2 px-md-2"
+          src={item.logo_url}
+          alt={item.title}
+        />
         <h5 className="d-none d-sm-block d-sm-none d-md-block">{item.title}</h5>
       </a>
     </div>
