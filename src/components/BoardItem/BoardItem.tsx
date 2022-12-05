b5292866aa41f4245c44d4e23776514be5893979
danyil <danilomaster2003@ukr.net>
2022-12-05 11:36:58 +0200

    fix: unintentional function trigger

--- src/components/BoardItem/BoardItem.tsx
+++ src/components/BoardItem/BoardItem.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { stringToColour } from '../../utils/colorFromString';
 import { IBoard } from '../../models';
 import { useDeleteBoardMutation } from '../../store/api/boardApi';
-import { Loader } from '../Loader/Loader';
 import Delete from '../../assets/images/icons/delete.png';
 import Edit from '../../assets/images/icons/edit.png';
 import { ModalComponent } from '../ModalComponent/ModalComponent';
@@ -17,11 +16,9 @@ interface IBoardItemProps {
 }
 
 export const BoardItem = ({ item }: IBoardItemProps) => {
-  const [deleteBoard, { isLoading }] = useDeleteBoardMutation();
+  const [deleteBoard] = useDeleteBoardMutation();
   const [editBoardModal, setEditBoardModal] = useState(false);
   const [showBoardDeleteModal, setShowBoardDeleteModal] = useState(false);
-
-  if (isLoading) return <Loader />;
 
   return (
     <div key={item._id} className="col-xl-3 col-md-3 col-sm-4 col-6">
@@ -62,17 +59,17 @@ export const BoardItem = ({ item }: IBoardItemProps) => {
                 </Button>
               </div>
             </div>
-            <ModalComponent
-              show={editBoardModal}
-              title={t('boards.modal.editing')}
-              onHide={() => setEditBoardModal(false)}
-              setModal={setEditBoardModal}
-            >
-              <EditBoardModal setEditBoardModal={setEditBoardModal} board={item}></EditBoardModal>
-            </ModalComponent>
           </div>
         </div>
       </Link>
+      <ModalComponent
+        show={editBoardModal}
+        title={t('boards.modal.editing')}
+        onHide={() => setEditBoardModal(false)}
+        setModal={setEditBoardModal}
+      >
+        <EditBoardModal setEditBoardModal={setEditBoardModal} board={item}></EditBoardModal>
+      </ModalComponent>
       <DeleteModal
         description={t('boards.modal.warning')}
         title={t('boards.modal.deleting')}
