9ea09ed097833a2260209bb67d7e07b945014870
ekater1na <ekaterina_l@tut.by>
2022-12-05 21:29:20 +0300

    feat: add margin

--- src/components/AuthSection/AuthSection.tsx
+++ src/components/AuthSection/AuthSection.tsx
@@ -48,21 +48,21 @@ export const AuthSection = () => {
                 onClick={() => setCreateBoardModal(true)}
                 title={t('auth.add-boards') || ''}
               >
-                <img className="px-sm-1" height="20" src={AddIcon} alt="auth.add-boards" />
+                <img className="px-sm-1 mb-1" height="20" src={AddIcon} alt="auth.add-boards" />
                 <span className="d-none d-sm-inline d-md-inline">{t('auth.add-boards')}</span>
               </Button>
             </NavLink>
 
             <NavLink className="px-sm-2 px-1" to="/">
               <Button variant="secondary btn-overflow" title={t('auth.to-main-page') || ''}>
-                <img className="px-sm-1" height="20" src={CheckIcon} alt="auth.to-main-page" />
+                <img className="px-sm-1 mb-1" height="20" src={CheckIcon} alt="auth.to-main-page" />
                 <span className="d-none d-sm-inline d-md-inline">{t('auth.to-main-page')}</span>
               </Button>
             </NavLink>
 
             <NavLink className="px-sm-2 px-1" to="/edit-profile">
               <Button variant="outline-secondary btn-overflow" title={t('auth.edit-profile') || ''}>
-                <img className="px-sm-1" height="20" src={EditIcon} alt="auth.edit-profile" />
+                <img className="px-sm-1 mb-1" height="20" src={EditIcon} alt="auth.edit-profile" />
                 <span className="d-none d-sm-inline d-md-inline">{t('auth.edit-profile')}</span>
               </Button>
             </NavLink>
@@ -73,7 +73,7 @@ export const AuthSection = () => {
                 onClick={onClickHandler}
                 title={t('auth.sign-out') || ''}
               >
-                <img className="px-sm-2 px-1" height="20" src={LogoutIcon} alt="auth.sign-out" />
+                <img className="px-sm-2 mb-1" height="20" src={LogoutIcon} alt="auth.sign-out" />
                 <span className="d-none d-sm-inline d-md-inline">{t('auth.sign-out')}</span>
               </Button>
             </Nav.Item>
