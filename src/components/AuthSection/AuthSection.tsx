a494d3d2f28c9fb1ff50514cefc10781a41474d0
ekater1na <ekaterina_l@tut.by>
2022-12-05 16:05:24 +0300

    feat: update icons

--- src/components/AuthSection/AuthSection.tsx
+++ src/components/AuthSection/AuthSection.tsx
@@ -6,6 +6,10 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { isAuthenticated } from '../../utils/isAuthenticated';
 import { ModalComponent } from '../ModalComponent/ModalComponent';
 import { CreateBoardModal } from '../CreateBoard/CreateBoardModal';
+import LogoutIcon from '../../assets/images/header-icons/logout.png';
+import AddIcon from '../../assets/images/header-icons/add-file.png';
+import CheckIcon from '../../assets/images/header-icons/checked.png';
+import EditIcon from '../../assets/images/header-icons/edit-user.png';
 
 export const AuthSection = () => {
   const { t } = useTranslation();
@@ -40,22 +44,28 @@ export const AuthSection = () => {
           <Navbar>
             <NavLink className="mr-10 px-2" to="/boards">
               <Button variant="secondary btn-overflow" onClick={() => setCreateBoardModal(true)}>
+                <img className="px-1" height="20" src={AddIcon} alt="auth.add-boards" />
                 {t('auth.add-boards')}
               </Button>
             </NavLink>
 
             <NavLink className="px-2 lh-sm" to="/">
-              <Button variant="secondary btn-overflow"> {t('auth.to-main-page')}</Button>
+              <Button variant="secondary btn-overflow">
+                <img className="px-1" height="20" src={CheckIcon} alt="auth.to-main-page" />
+                {t('auth.to-main-page')}
+              </Button>
             </NavLink>
 
             <NavLink className="px-2" to="/edit-profile">
-              <Button variant="outline-secondary w-max-25 btn-overflow">
+              <Button variant="outline-secondary btn-overflow">
+                <img className="px-1" height="20" src={EditIcon} alt="auth.edit-profile" />
                 {t('auth.edit-profile')}
               </Button>
             </NavLink>
 
             <Nav.Item>
               <Button className="btn-overflow" onClick={onClickHandler}>
+                <img className="px-1" height="20" src={LogoutIcon} alt="auth.sign-out" />
                 {t('auth.sign-out')}
               </Button>
             </Nav.Item>
